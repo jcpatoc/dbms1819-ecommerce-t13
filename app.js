2ede121d24650b9834dfbaf50cc05502156ed541
unknown <patocjoanne9@gmail.com>
2018-08-10 12:14:19 +0800

    first commit

--- app.js
+++ app.js
@@ -274,5 +274,5 @@ app.post('/order', function (req, res) {
 });
 
 app.listen(app.get('port'), function(){
-  console.log('Server started on port' + app.get('port'))
+  console.log('Server started on port' +app.get('port'))
 });
